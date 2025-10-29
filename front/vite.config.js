ae523df7b62c09caa7a67c7aefec5bd9a7aeb8a4
botoli <standrey2007@gmail.com>
2025-10-30 02:00:26 +0300

    fix vercel bug

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,13 +1,12 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000, // Попробуйте вернуться к 3000
-    strictPort: false, // Разрешить использовать другой порт если 3000 занят
-    host: true,
-    open: true,
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
   },
 });
